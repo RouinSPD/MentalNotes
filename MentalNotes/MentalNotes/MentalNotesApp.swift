6fc459e6b2efa32a659430abb857dcabe11cf3a4
RouinSPD <128640701+RouinSPD@users.noreply.github.com>
2024-03-25 20:07:43 +0100

    Xcode file

---
+++ MentalNotes/MentalNotes/MentalNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalNotesApp.swift
+//  MentalNotes
+//
+//  Created by Pedro Daniel Rouin Salazar on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MentalNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
